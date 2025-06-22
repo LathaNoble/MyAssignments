ea9d461d0d17594f9995f7236f26a5a7f466069c
LathaNoble <lathathurai90@gmail.com>
2025-06-22 13:52:42 +0530

    Add files via upload
    Day1

---
+++ day1/LearnDataType.java
@@ -0,0 +1,9 @@
+package week1.day1;
+
+public class LearnDataType {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to TestLeaf");
+	}
+
+}
