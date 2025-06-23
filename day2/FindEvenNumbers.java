5da295ba4a6cb5edbfb9b0a4390659c1b997fb75
LathaNoble <lathathurai90@gmail.com>
2025-06-23 21:39:04 +0530

    Add files via upload

---
+++ day2/FindEvenNumbers.java
@@ -0,0 +1,17 @@
+package week1.day2;
+
+public class FindEvenNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int maxrange=10;
+		for (int i=1;i<=maxrange;i++)
+		{
+			if (i%2==0)
+			{
+				System.out.println("Number is Even "+i);
+			}
+		}
+	}
+
+}
---
+++ day2/FindOddNumbers.java
@@ -0,0 +1,17 @@
+package week1.day2;
+
+public class FindOddNumbers {
+	
+	public static void main(String[] args)
+	{
+		int maxrange=10;
+		for (int i=1;i<=maxrange;i++)
+		{
+			if (i%2!=0)
+			{
+				System.out.println("Number is Odd "+i);
+			}
+		}
+	}
+
+}
